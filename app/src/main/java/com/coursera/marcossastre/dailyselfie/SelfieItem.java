9db5621d0455092efe6adc45725762c1208c8e08
Marcos Sastre <marcossastre73@gmail.com>
2015-11-09 01:31:20 -0300

    Created SelfieItem class and selfie_item layout

---
+++ app/src/main/java/com/coursera/marcossastre/dailyselfie/SelfieItem.java
@@ -0,0 +1,19 @@
+package com.coursera.marcossastre.dailyselfie;
+
+import android.graphics.Bitmap;
+
+import java.io.File;
+
+/**
+ * Created by Marcos Sastre on 09/11/2015.
+ */
+public class SelfieItem {
+    //Thumbnail returned from the cam
+    private Bitmap thumb;
+    //Title to display
+    private String title;
+    //Path to the full imagen saved by the cam
+    private File fullImage;
+    //Button to delete the current
+
+}
